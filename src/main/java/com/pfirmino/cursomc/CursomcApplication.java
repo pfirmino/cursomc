014f9bd3eba1dc984577c035f1352bdbbd3dc67e
PedroFirmino <pedrofirmino1@hotmail.com>
2020-10-02 14:01:10 -0300

    Adicionado o comentário de configuração da Porta

--- src/main/java/com/pfirmino/cursomc/CursomcApplication.java
+++ src/main/java/com/pfirmino/cursomc/CursomcApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication {
+	/*
+		Para alterar a porta, adicione a chave abaixo no arquivo application.properties
 
+		server.port=${port:8081}
+	
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
