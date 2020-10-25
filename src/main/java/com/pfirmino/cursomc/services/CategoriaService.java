1b892eff9ffd839eec22e5fd0e4fbd9e380fe47f
pfirmino <pedrofirmino@hotmail.com>
2020-10-25 20:32:26 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/pfirmino/cursomc/services/CategoriaService.java
@@ -0,0 +1,18 @@
+package com.pfirmino.cursomc.services;
+
+import java.util.Optional;
+import com.pfirmino.cursomc.domain.Categoria;
+import com.pfirmino.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
