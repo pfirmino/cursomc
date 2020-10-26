121f73feb7794112fcef853084d605e00fc045ea
pfirmino <pedrofirmino@hotmail.com>
2020-10-26 15:45:16 -0300

    Criando operação de instanciação

--- src/main/java/com/pfirmino/cursomc/CursomcApplication.java
+++ src/main/java/com/pfirmino/cursomc/CursomcApplication.java
@@ -1,18 +1,33 @@
 package com.pfirmino.cursomc;
 
+import java.util.Arrays;
+
+import com.pfirmino.cursomc.domain.Categoria;
+import com.pfirmino.cursomc.repositories.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 	/*
 		Para alterar a porta, adicione a chave abaixo no arquivo application.properties
 
 		server.port=${port:8081}
 	
 	*/
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
