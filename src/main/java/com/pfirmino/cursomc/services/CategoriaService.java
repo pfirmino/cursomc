01b14710dcfb1dab4e3ca08529b3c66877248c90
PedroFirmino <pedrofirmino1@hotmail.com>
2020-10-30 15:09:47 -0300

    Ajustes no Endpoint /categorias/{id}

--- src/main/java/com/pfirmino/cursomc/services/CategoriaService.java
+++ src/main/java/com/pfirmino/cursomc/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.pfirmino.cursomc.domain.Categoria;
 import com.pfirmino.cursomc.repositories.CategoriaRepository;
+import com.pfirmino.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+         "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));        
     }
 }
