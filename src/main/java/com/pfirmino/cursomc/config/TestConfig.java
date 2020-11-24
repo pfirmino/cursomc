5e26df4ed454a12f9bfa94394ff7d4160a81ba8d
PedroFirmino <pedrofirmino1@hotmail.com>
2020-11-24 17:53:08 -0300

    Criação do profile de teste

---
+++ src/main/java/com/pfirmino/cursomc/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.pfirmino.cursomc.config;
+
+import com.pfirmino.cursomc.services.DBService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+    
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDataBase() throws Exception {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
