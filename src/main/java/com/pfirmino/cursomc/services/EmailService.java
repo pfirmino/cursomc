a9bd905fbd73293a2291d4d16eab32e8298ffce7
PedroFirmino <pedrofirmino1@hotmail.com>
2020-11-26 15:28:25 -0300

    Email HTML

--- src/main/java/com/pfirmino/cursomc/services/EmailService.java
+++ src/main/java/com/pfirmino/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.pfirmino.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import com.pfirmino.cursomc.domain.Pedido;
 
 import org.springframework.mail.SimpleMailMessage;
@@ -7,4 +9,6 @@ import org.springframework.mail.SimpleMailMessage;
 public interface EmailService {
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 }
